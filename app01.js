162bae075812cfaca15ca9b80c6332bf2d140a69
Shakti Tiwari <shaktitiwari1182511@gmail.com>
2022-10-11 20:52:41 +0530

    changes

---
+++ app01.js
@@ -0,0 +1,5 @@
+console.log('Hello Javascript');
+var a = 'hello world';
+function b(){console.log('this is func. b');
+}
+var d=10;
---
+++ app22.js
@@ -0,0 +1,6 @@
+// local storage
+localStorage.setItem('product', 'laptop');
+
+//reterive item fromm local storage
+// var prod=localStorage.getItem('product');
+// console.log(prod);
